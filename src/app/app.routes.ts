23ef9ad1bfccaf0ebf191a7cb5162ac10dedb512
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-03-21 21:03:48 +0530

    email verification UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,6 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './page/landing/landing.component';
 
 export const routes: Routes = [
-    {
-        path: "",
-        component: LandingComponent
-    },
+    { path: "", component: LandingComponent },
+    { path: "landing", component: LandingComponent }
 ];
